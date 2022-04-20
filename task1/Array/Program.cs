a92bf6038940556e2f3a5d2457d036c7518ee8bf
Jahanda97 <jahangirjn@code.edu.az>
2022-04-20 09:17:48 +0400

    first commit

---
+++ task1/Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soz sayini daxil et:");
+            int wordsCount = Convert.ToInt32(Console.ReadLine());
+            string[] words = new string [wordsCount];
+            Console.WriteLine("Sozu daxil et:");
+            int i;
+            for (i = 0; i < wordsCount; i++)
+            {
+                words[i] = Console.ReadLine();
+            }
+            Console.WriteLine("\n");
+            foreach (string str in words)
+            {
+                if (str.Length > 5)
+                {
+                    Console.WriteLine(str);
+                }
+            }
+        }
+    }
+}
---
+++ task11/SumofDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" eded daxil edin:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (n != 0)
+            {
+                sum = n % 10 + sum;
+                n = n / 10;
+            }
+            Console.WriteLine("\ndaxil edilmis ededin reqemlerinin cemi:\n" + sum);
+        }
+    }
+}
